c7b5562ba4c5dfddcb84980563aea59ea1812b60
rvijayagopalan-coder <rvijayagopalan@hotmail.com>
2025-07-14 14:20:16 -0400

    Terraform Fmt Changes

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,20 +1,20 @@
 locals {
-    labels = {
-        "data-project" = var.data-project
-    }
+  labels = {
+    "data-project" = var.data-project
+  }
 }
 
 variable "project" {
-    type= string
-    description = "ID Google project"
+  type        = string
+  description = "ID Google project"
 }
 
 variable "region" {
-    type= string
-    description = "Region Google project"
+  type        = string
+  description = "Region Google project"
 }
 
-variable  "data-project" {
-    type = string
-    description = "Name of data pipeline project to use as resource prefix"
+variable "data-project" {
+  type        = string
+  description = "Name of data pipeline project to use as resource prefix"
 }
--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "gcs" { 
-      bucket  = "terraform-state-jb-cicdproject-gpc-poc"
-      prefix  = "prod"
-    }
+  backend "gcs" {
+    bucket = "terraform-state-jb-cicdproject-gpc-poc"
+    prefix = "prod"
+  }
 }
 
 provider "google" {
   project = var.project
-  region = var.region
+  region  = var.region
 }
